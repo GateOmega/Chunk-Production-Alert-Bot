d078af532fa0bea58a166b31f5f3f77d605b0cdd
GateOmega <109391990+GateOmega@users.noreply.github.com>
2022-08-20 01:22:45 +0300

    Create uptime-alert.sh

---
+++ uptime-alert.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CURRENT=$(near validators current |grep <VALIDATOR-NAME> | awk -F'|' '{ print $5}' | sed 's/%//g')
+THRESHOLD=90
+
+MESSAGE="Chunk Uptime Check: $CURRENT%"
+
+
+if [ "$CURRENT" -lt "$THRESHOLD" ] ; then
+curl -X POST \
+-H 'Content-Type: application/json' \
+-d '{"chat_id": "<YOUR-TELEGRAM-CHAT-ID>", "text": "'"$MESSAGE"'", "disable_notification": true}' \
+https://api.telegram.org/<YOUR-TELEGRAM-BOT-TOKEN>/sendMessage
+fi
+
+#export NEAR_ENV=shardnet
+#export LOGS=/home/$USER/uptimelogs
+
+echo "---" >> /home/<$USER>/uptimelogs/all.log
+date >> /home/<$USER>/uptimelogs/all.log
+echo $CURRENT >> /home/<$USER>/uptimelogs/all.log
